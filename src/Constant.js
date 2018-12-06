04bc4f2ba07d27cb007e1969e5e2abf53e03fdfc
codingtoc <codingtoc@gmail.com>
2018-12-06 21:36:47 +0900

    변이 또는 액션 관련 메서드 이름 정의(src/Constant.js)

---
+++ src/Constant.js
@@ -0,0 +1,11 @@
+// 변이 또는 액션 관련 메서드 이름 정의
+export default {
+  // 할일 목록 상태 변경(변이, 액션)
+  FETCH_TODOS: "fetchTodos",
+  // 신규 할일 추가(액션)
+  ADD_TODO: "addTodo",
+  // 기존 할일 완료여부 변경(액션)
+  DONE_TOGGLE: "doneToggle",
+  // 기존 할일 삭제(액션)
+  DELETE_TODO: "deleteTodo"
+};
