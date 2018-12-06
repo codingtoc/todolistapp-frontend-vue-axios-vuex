6565bf0fcc9f3491bbe91889417f427947dd455b
codingtoc <codingtoc@gmail.com>
2018-12-06 23:53:58 +0900

    Vue 인스턴스 생성 시 저장소 옵션 추가(src/main.js 변경)

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+// vue 모듈 가져오기
+import Vue from "vue";
+// App 컴포넌트 가져오기
+import App from "./App.vue";
+// Vuex의 저장소 가져오기
+import store from "./store";
 
+// Vue 인스턴스 생성
 new Vue({
-  el: '#app',
+  // 저장소 옵션
+  store,
+  // HTML 요소 옵션
+  el: "#app",
+  // App 컴포넌트 렌더링 메서드 옵션
   render: h => h(App)
-})
+});
